62cc36f9ac8b6956799c334bcc74ecf4a5a75bca
LeoRoA <romanleontev@gmail.com>
2022-09-22 19:12:28 +0700

    На проверку ДЗ №2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Задание 1");
+        for (int i=1904; i<=2096; i = i + 4){
+            System.out.println("Високосный год: "+i);
+        }
+
+        //Задание 2
+        System.out.println("Задание 2");
+        for (int i=1; i<=14; i++) {
+            System.out.println(i * 7);
+        }
+
+        //Задание 3
+        System.out.println("Задание 3");
+        for (int i=0; i<=9; i++) {
+            int k = (int) Math.pow (2, i);
+            System.out.println(k);
+        }
     }
 }
